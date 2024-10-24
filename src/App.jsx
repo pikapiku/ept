2eb52f900485dbf345b433e418ed73d1aaf5973b
Deki <z5268870@student.unsw.edu.au>
2024-10-24 23:29:55 +1100

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
 		if (!showTotal) {
 			return (
 				<div className="card">
-					<p><i>This is the official e-dating quiz that will evaluate your e-dating score. The questions cover all aspects of the online dating world, from the most common to the most extreme experiences. <u>Warning</u>: This quiz references self harm.</i> </p>
+					<p><i>The official e-dating quiz that will evaluate your e-dating score. The questions cover all aspects of the online dating world, from the most common to the most extreme experiences. <u>Warning</u>: This quiz references self harm.</i> </p>
 					<br />
 					<p><b>Disclaimer: these questions <u>ONLY</u> apply to the online environment.<br />&apos;them&apos; refers to an online partner or love interest.</b></p>
 					<br />
@@ -64,7 +64,7 @@ function App() {
 		} else {
 			return (
 				<div className="card">
-					<p><i>This is the official e-dating quiz that will evaluate your e-dating score. The questions cover all aspects of the online dating world, from the most common to the most extreme experiences.</i></p>
+					<p><i>The official e-dating quiz that will evaluate your e-dating score. The questions cover all aspects of the online dating world, from the most common to the most extreme experiences.</i></p>
 				</div>
 			)
 		}
